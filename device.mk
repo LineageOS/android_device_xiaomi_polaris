88f8cccc3e269509b88c77c667f423cbd1afa353
Ruchi Kandoi <kandoiruchi@google.com>
2018-03-01 14:09:08 -0800

    polaris: Remove system and vendor image dependency
    libnfc-nci.conf shouldn't be a part of vendor image, since it is
    accessed by libnfc-nci and Nfc application, both are part of system
    image.
    
    Test: Nfc enable/disable
    Bug: 72080121
    Change-Id: I03b6387afc35a521c8a6f9fea1ec2e649d56a38f

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Secure element
