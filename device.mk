273c84c07a11dccc5bcbc095ff12a772800c04ad
Bruno Martins <bgcngm@gmail.com>
2018-09-10 23:42:16 +0100

    polaris: Move to common P blobs and drop all device-specific treble hacks
     * Android 9 (P) is now available as public beta, meaning that all the hacks
       for running 8.1 with 8.0 blobs can now be dropped. The system blobs
       are now imported in the sdm845-common tree.
    
    Change-Id: I68595dd790dec51fb45ccd1d3fb907be81f3b896

--- device.mk
+++ device.mk
@@ -28,18 +28,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Inherit from sdm845-common
 $(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
 
-# Audio
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
-
-# Device init scripts
-PRODUCT_PACKAGES += \
-    init.target.rc
-
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/compatibility_matrix.xml:system/compatibility_matrix.xml
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
@@ -49,8 +37,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
-
-# VNDK compat
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vndk-compat/ld.config.compat.txt:system/etc/ld.config.compat.txt \
-    $(LOCAL_PATH)/vndk-compat/vndk-compat.rc:system/etc/init/vndk-compat.rc
