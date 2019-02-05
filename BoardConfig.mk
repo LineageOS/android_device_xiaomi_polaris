ba8c34fa551bce6c9e455226f40e3c786e192175
Bruno Martins <bgcngm@gmail.com>
2019-02-05 11:44:55 +0000

    polaris: Declare supported LiveDisplay 2.0 interfaces
     * The framework manifest must now be split per device, since not
       every interface is supported. In this case, only DisplayModes
       and PictureAdjustment are supported.
    
    Change-Id: Ie2bbb02ab99ae5f4654fd1e459d2919941197040

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ TARGET_OTA_ASSERT_DEVICE := polaris
 # Kernel
 TARGET_KERNEL_CONFIG := polaris_defconfig
 
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
 # NFC
 TARGET_USES_NQ_NFC := true
 
