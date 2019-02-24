514a991c5b09eb2e8e01bc2c6843efb71773bddd
Bruno Martins <bgcngm@gmail.com>
2019-02-24 10:44:55 +0000

    polaris: Set device stock props
    Change-Id: Ia8ccea7a370be0ce3cca8073b770f0249354204b

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,9 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.recording.hd=true \
+    ro.vendor.audio.sos=true \
+    ro.vendor.audio.us.proximity=true
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
@@ -15,3 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.eyecare.brightness.threshold=15 \
     ro.eyecare.brightness.level=8 \
     ro.whitepoint_calibration_enable=false
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default
