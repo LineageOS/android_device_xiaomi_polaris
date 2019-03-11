570c970a5882d656d4a0ecce8a56b1d09f44ebe8
Fugazzina <47015480+Fugazzina@users.noreply.github.com>
2019-03-11 18:51:29 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ DEVICE_PATH := device/xiaomi/polaris
 TARGET_OTA_ASSERT_DEVICE := polaris
 
 # Kernel
-TARGET_KERNEL_CONFIG := polaris_defconfig
+TARGET_KERNEL_CONFIG := simple-polaris_defconfig
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
