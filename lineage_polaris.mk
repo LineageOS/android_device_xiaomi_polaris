dca62446a9027b02502a5b1372b302f40e3d9d69
palaych <palaychm@yandex.ru>
2019-03-17 09:25:14 +0000

    polaris: Update to stable Pie fingerprint
    Change-Id: Idd9a571be1d11493a25be974b624e181c4e02085

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -16,10 +16,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:9/PKQ1.180729.001/V10.2.2.0.PDGMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
+    PRIVATE_BUILD_DESC="polaris-user 9 PKQ1.180729.001 V10.2.2.0.PDGMIXM release-keys" \
     PRODUCT_NAME="polaris" \
     TARGET_DEVICE="polaris"
 
