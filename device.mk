902b78a83c937a1caed6eab59f0c4c5c9a432463
LuK1337 <priv.luk@gmail.com>
2020-10-29 14:31:02 +0100

    Revert "polaris: Exclude lineage-sdk overlays from RRO"
    This reverts commit 087df90f1ea64d6ee910dfe08bbfb0f8f8f4ec5e.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
