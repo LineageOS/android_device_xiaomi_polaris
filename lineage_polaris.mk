b5b908b8e9ce0c319b4dcadfe9a627ad47580b36
LuK1337 <priv.luk@gmail.com>
2022-01-27 10:53:26 +0100

    polaris: Make device name/product prop overrides work
    Change-Id: I64d11103ddb8f9dce70b9bc110745d4aafd667dc

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2019,2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,10 +16,12 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
 
+PRODUCT_SYSTEM_NAME := dipper
+
 BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
-    PRODUCT_NAME="polaris"
+    TARGET_PRODUCT="polaris"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
