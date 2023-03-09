b605e6900a560f009f5978baaaf31cd74513656f
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    polaris: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- device.mk
+++ device.mk
@@ -96,16 +96,11 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
     Tag
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
-
-# Secure element
-PRODUCT_PACKAGES += \
-    SecureElement
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
