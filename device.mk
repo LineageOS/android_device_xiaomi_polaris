dd0863696ff0c5a6c206bfbcfd2bbcdab9c57295
ShevT <evgeny.shishkov@gmail.com>
2024-08-07 08:42:43 +0300

    polaris: Switch to AIDL NXP NFC HAL
    Change-Id: Iff03bd7fc522b45b9caebb12860c5c851356a9a1

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     libchrome.vendor \
     Tag
